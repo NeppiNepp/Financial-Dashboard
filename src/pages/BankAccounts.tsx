e449c4462f2adb42a5202140a46c4a6c235e70fc
Gage Scarberry <gagescarberry@aol.com>
2025-08-15 17:21:24 -0400

    Changes for Deployment

--- src/pages/BankAccounts.tsx
+++ src/pages/BankAccounts.tsx
@@ -32,7 +32,9 @@ export default function Accounts({
         let name: string, balance: string, limit: string, goal: string, rewards: string;
         switch (type) {
             case 'Checking':
+                // @ts-ignore
                 name = nameRef.current.value;
+                // @ts-ignore
                 balance = balanceRef.current.value;
                 updateCurrentCheckingInfo((draft: any[]) => {
                     draft.push({
@@ -47,9 +49,13 @@ export default function Accounts({
                 setAddingAccount(false);
                 break;
             case 'Credit':
+                // @ts-ignore
                 name = nameRef.current.value;
+                // @ts-ignore
                 balance = balanceRef.current.value;
+                // @ts-ignore
                 limit = limitRef.current.value;
+                // @ts-ignore
                 rewards = rewardsRef.current.value;
                 updateCurrentCreditInfo((draft: any[]) => {
                     draft.push({
@@ -66,8 +72,11 @@ export default function Accounts({
                 setAddingAccount(false);
                 break;
             case 'Savings':
+                // @ts-ignore
                 name = nameRef.current.value;
+                // @ts-ignore
                 balance = balanceRef.current.value;
+                // @ts-ignore
                 goal = goalRef.current.value;
                 updateCurrentSavingsInfo((draft: any[]) => {
                     draft.push({
